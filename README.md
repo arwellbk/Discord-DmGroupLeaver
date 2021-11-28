205ffa2d5507c771688ac5ac3c374d8f3d79f0f2
Burak <90971761+arwellbk@users.noreply.github.com>
2021-11-28 12:40:55 +0300

    Discord Group Leaver
    made by arwell#4774

---
+++ README.md
@@ -0,0 +1,11 @@
+**Hi there, it's a simple discord group leave script.**
+
+_To use,_
+<ul>
+<li>Execute the command: pip install discord</li>
+<li>Get your accounts token.</li>
+<li>Enter the ID numbers of the groups to be bypassed on line 19.</li>
+<li>Enter your token on line 25.</li>
+<li>Execute the command: python main.py</li>
+<li>Have fun!</li>
+</ul>
---
+++ main.py
@@ -0,0 +1,29 @@
+import discord
+bot = discord.Client()
+
+
+@bot.event
+async def on_ready():
+    print("Use .lv command to leave groups.")
+
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        command = message.content.strip().split(' ')
+        cmd = command[0]
+        if cmd == ".lv":
+            await message.delete()
+            count = 0
+            for c in bot.private_channels:
+                if isinstance(c, discord.GroupChannel):
+                    byp = []  # Enter the IDs of the groups to be bypassed
+                    if not c.id in byp:
+                        count = count + 1
+                        await c.leave()
+            print("Success")
+            await bot.close()
+TOKEN = "your accounts token goes here"
+bot.run(TOKEN, bot=False)
+
+'''https://github.com/arwellbk'''
